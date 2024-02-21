9e16e9243bbec885f74a73234aa4164c80ccb0bf
VladimirCh <vladimir.chechurov@gmail.com>
2024-02-21 15:59:48 +0300

    1

--- src/main/kotlin/hse.ru.vladch/controllers/ConsoleControllerVisitor.kt
+++ src/main/kotlin/hse.ru.vladch/controllers/ConsoleControllerVisitor.kt
@@ -1,7 +1,88 @@
 package hse.ru.vladch.controllers
 
-class ConsoleControllerVisitor : Controller {
+import hse.ru.vladch.dao.InMemoryMenuItemDao
+
+class ConsoleControllerVisitor(val menuInit : InMemoryMenuItemDao) : Controller {
+    private val menu = menuInit
     override fun launch() {
         TODO("Not yet implemented")
+    }
+
+    private fun printMenu() {
+        println("Select the option:")
+        println("1 - View menu")
+        println("2 - Make an order")
+        println("3 - Add dish to the last order")
+        println("4 - View all previous orders")
+        println("5 - View the last order")
+        println("6 - Cancel last order")
+        print("7 - Exit program")
+        var ans = 0
+        try {
+            ans = readln().toInt()
+        } catch (e : Exception) {
+            println("Wrong input! Try again")
+            printMenu()
+        }
+        if ((ans < 1) || (ans > 7)) {
+            println("Incorrect input! Try again...")
+            printMenu()
+        }
+        when (ans) {
+            1 -> {
+                printMenuItems()
+            }
+            2 -> {
+                createOrder()
+            }
+            3 -> {
+                addDishToLastOrder()
+            }
+            4 -> {
+                getAllOrders()
+            }
+            5 -> {
+                getLastOrder()
+            }
+            6 -> {
+                cancelLastOrder()
+            }
+            7 -> {
+                exitToAuthorizationMenu()
+            }
+        }
+    }
+
+    private fun printMenuItems() {
+        try {
+            println(menu.getAllDishesString())
+        } catch (e : Exception) {
+            println(e.message)
+        }
+        printMenu()
+    }
+
+    private fun createOrder() {
+        TODO()
+    }
+
+    private fun addDishToLastOrder() {
+        TODO()
+    }
+
+    private fun getAllOrders() {
+        TODO()
+    }
+
+    private fun getLastOrder() {
+        TODO()
+    }
+
+    private fun cancelLastOrder() {
+
+    }
+
+    private fun exitToAuthorizationMenu() {
+        TODO()
     }
 }
--- src/main/kotlin/hse.ru.vladch/service/KitchenService.kt
+++ src/main/kotlin/hse.ru.vladch/service/KitchenService.kt
@@ -2,4 +2,9 @@ package hse.ru.vladch.service
 
 interface KitchenService {
     fun createOrder()
+    fun addDishToOrder()
+    fun cancelOrder()
+    fun getOrdersOfVisitor()
+    
+
 }
